28186f99e9b445dff47a44718e876cdb78640f61
Joya <privategafbusiness@gmail.com>
2025-03-17 22:39:56 +0530

    some bugs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/devboard/json/repo_list.json")  // JSON file fetch kar rahe hain
+    fetch("https://urnjoya.github.io/devboard/json/repo_list.json")  // JSON file fetch kar rahe hain
         .then(response => response.json())
         .then(data => {
             const container = document.getElementById("repo-container");
-           console.log(container);  // Debugging line to check if container is selected
-
+            console.log(container, "container data");  // Debugging line to check if container is selected
             if (!container) {
                 console.error("Container element not found");
+            }
             data.forEach(repo => {
                 const card = document.createElement("div");
                 card.classList.add("repo-card");
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <a href="${repo.repo_link}" target="_blank">GitHub Repo</a> |
                     <a href="${repo.live_link}" target="_blank">Live Demo</a>
                 `;
-    console.log(container,"|||",card);
+                console.log(container, "|||", card);
                 container.appendChild(card);
             });
         })
