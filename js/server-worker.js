b3cd90d8e844e36652a42b72133f77bbba961fd7
Joya <urnjoya@gmail.com>
2025-04-13 09:17:56 +0000

    Update server-worker.js

--- js/server-worker.js
+++ js/server-worker.js
@@ -3,16 +3,16 @@ self.addEventListener("install", (e) => {
     caches.open("devboard-cache").then((cache) =>
       cache.addAll([
         "/",
-        "/index.html",
-        "/css/style.css",
-        "/css/nav_style.css",
-        "/css/footer_style.css",
-        "/js/script.js",
-        "/js//no_change.js",
-        "/js/force_refresh.js",
+        "index.html",
+        "css/style.css",
+        "css/nav_style.css",
+        "css/footer_style.css",
+        "js/script.js",
+        "js/no_change.js",
+        "js/force_refresh.js",
         "json//manifest.json",
-        "/json/devboard_small.png",
-        "/json/devboard_large.png"
+        "json/devboard_small.png",
+        "json/devboard_large.png"
         // Add other assets here
       ])
     )
